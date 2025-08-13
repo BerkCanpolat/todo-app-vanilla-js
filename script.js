58b4ec1cb94e2910324e99e9b22d7fa720325942
Berk Canpolat <canpolatberk19@gmail.com>
2025-08-13 06:22:46 +0300

    The function to add tasks to the to-do list has been written. And the bug in the total task count has been fixed

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Nav menu dropdown
 const secretMenu = document.querySelector(".main-header__secret-menu");
 const secretMenuClick = document.querySelector(".header-h1");
 
@@ -18,4 +19,104 @@ function checkCloseMenu() {
     menuTimer = setTimeout(() => {
         secretMenu.classList.remove("secret-menu-js");
     }, 200);
+}
+
+
+// Todo-Lists
+const todoLists = document.getElementById("todo-lists");
+const lengthTodo = document.querySelector(".span-length");
+const formAdd = document.getElementById("form-add");
+
+loadElements();
+
+formAdd.addEventListener("submit", formHandle);
+
+function loadElements() {
+    const items = [
+        {id:1, text: "Ben yazdım", completed: false},
+        {id:2, text: "Beşiktaş ulan", completed: true},
+        {id:3, text: "Olcak mı", completed: false},
+        {id:4, text: "Length test", completed: false},
+    ];
+
+
+    lengthTodo.textContent = items.length ? `${items.length}` : "load items";
+    
+
+    for(let todo of items) {
+        const li = createElemen(todo);
+        todoLists.appendChild(li);
+    }
+}
+
+function createElemen(item) {
+    const li = document.createElement("li");
+    li.classList.add("ul-main__li-submain");
+
+    const input = document.createElement("input");
+    input.classList.add("li-submain__input");
+    input.type = "checkbox";
+    input.placeholder = "Add a new task";
+    input.checked = item.completed;
+
+    const div = document.createElement("div");
+    div.classList.add("li-submain__text");
+    div.textContent = item.text;
+
+    const i = document.createElement("i");
+    i.className = "fa-solid fa-circle-minus delete-btn";
+    i.addEventListener("click", trashBtn);
+
+    li.appendChild(input);
+    li.appendChild(div);
+    li.appendChild(i);
+    return li;
+}
+
+function formHandle(e) {
+    e.preventDefault();
+    console.log(e.target);
+
+    const input = document.getElementById("input-add");
+
+    if(input.value.trim().length == 0) {
+        alert("Birşey girmelisin");
+        return;
+    }
+
+    addTask(input);
+    
+}
+
+function addTask(input) {
+    const item = createElemen({
+        id: generateId(),
+        text: input.value,
+        completed: false,
+    });
+
+    
+    todoLists.appendChild(item);
+    
+    input.value = "";
+
+    lengthTodo.textContent = todoLists.children.length ? `${todoLists.children.length}` : "Not Todos";
+
+    console.log(`noli burda span ${lengthTodo.textContent}`); // doğru elementi seçiyor musun?
+console.log(todoLists.children.length); // sayı doğru mu?
+
+}
+
+function generateId() {
+    return Date.now().toString();
+}
+
+function trashBtn(e) {
+    console.log(e.target);
+    const trash = e.target.parentElement;
+
+    todoLists.removeChild(trash);
+
+    lengthTodo.textContent = todoLists.children.length ? `${todoLists.children.length}` : "Not Todos";
+    
 }
